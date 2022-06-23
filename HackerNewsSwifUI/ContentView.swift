2b03675ba4afcb0f01e43a73f2d19ec6aa269766
Marymezh <maria.mezhova@yahoo.com>
2022-06-23 14:28:25 +0700

    creating a SwiftUI List to populate the contents of the posts array

--- HackerNewsSwifUI/ContentView.swift
+++ HackerNewsSwifUI/ContentView.swift
@@ -9,10 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List(posts) { post in
+                Text(post.title)
+            }
+            .navigationBarTitle("Hacker News")
+        }
     }
 }
+
+struct Post: Identifiable {
+    let id: String
+    let title: String
+    
+}
+
+let posts = [
+Post(id: "1", title: "Hello"),
+Post(id: "2", title: "Bonjour"),
+Post(id: "3", title: "Hola!")
+]
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
