dfa237ef2e45187bac23f2a37e5199f8327a177e
Marymezh <maria.mezhova@yahoo.com>
2022-06-23 14:07:52 +0700

    Initial Commit

---
+++ HackerNewsSwifUI/HackerNewsSwifUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsSwifUIApp.swift
+//  HackerNewsSwifUI
+//
+//  Created by Мария Межова on 23.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsSwifUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
